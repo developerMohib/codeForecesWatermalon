eafa5bc0dec55505a8507799246d089a0f364167
Mohib <92154638+developerMohib@users.noreply.github.com>
2023-06-07 00:05:31 +0600

    it's my belated. i have to give fine for this

---
+++ app.js
@@ -0,0 +1,9 @@
+// even odd number selection problem from codefores
+
+let anyNumber = parseInt(prompt(`Enter your Number : `));
+
+if(anyNumber % 2 == 0){
+    console.log(`This Number is Even `)
+}else{
+    console.log(`This Number is Odd`)
+}
